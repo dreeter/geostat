05db7e8bba6863b0f29008384138bf3f506b87f4
Derek Reeter <dreeter00@gmail.com>
2022-08-02 18:50:09 -0600

    refactor with async/await, basic search and render capability

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,36 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,105 +38,131 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const https_1 = __importDefault(require("https"));
-const http_1 = __importDefault(require("http"));
+const http = __importStar(require("http"));
+const _ = __importStar(require("lodash"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 const port = 3000;
-let countries = {};
 let cityCountry = [];
 app.use(express_1.default.static('public'));
 app.set("view engine", "ejs");
-function getIPInfo(ipAddress) {
+function getAPIInfo(url, requestType) {
     return new Promise((resolve, reject) => {
-        ipAddress = "24.48.0.1";
-        const url = "http://ip-api.com/json/" + ipAddress;
-        http_1.default.get(url, (response) => {
-            if (response.statusCode !== 200) {
-                reject("Invalid Response Code: " + response.statusCode);
-            }
-            let responseData = "";
-            response.on("data", (data) => {
-                responseData += data;
+        if (requestType === "https") {
+            https_1.default.get(url, (response) => {
+                if (response.statusCode !== 200) {
+                    reject("Invalid Response Code: " + response.statusCode + "from " + url);
+                }
+                let responseData = "";
+                response.on("data", (data) => {
+                    responseData += data;
+                });
+                response.on("end", () => {
+                    resolve(JSON.parse(responseData));
+                });
             });
-            response.on("end", () => {
-                const ipInfo = JSON.parse(responseData);
-                resolve(ipInfo);
+        }
+        else {
+            http.get(url, (response) => {
+                if (response.statusCode !== 200) {
+                    reject("Invalid Response Code: " + response.statusCode + "from " + url);
+                }
+                let responseData = "";
+                response.on("data", (data) => {
+                    responseData += data;
+                });
+                response.on("end", () => {
+                    resolve(JSON.parse(responseData));
+                });
             });
-        });
+        }
     });
 }
-function getWeatherInfo(data) {
-    return new Promise((resolve, reject) => {
-        const apiKey = "c0b5e29a902014b259dad2f832ededd7";
-        const userCity = data.city;
-        const userCountry = data.country;
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&userCountry=" + userCountry + "&appid=" + apiKey + "&units=imperial";
-        https_1.default.get(url, (response) => {
-            if (response.statusCode !== 200) {
-                reject("Invalid Response Code: " + response.statusCode);
-            }
-            let responseData = "";
-            response.on("data", (data) => {
-                responseData += data;
-            });
-            response.on("end", () => {
-                const jsonWeatherData = JSON.parse(responseData);
-                const weatherInfo = jsonWeatherData.main;
-                resolve(weatherInfo);
-            });
-        });
-    });
+function formatInformation(data) {
+    const info = [
+        {
+            cardTitle: "Weather",
+            listItemOne: "Temperature: " + data[1].main.temp,
+            listItemTwo: "Description: " + data[1].weather.description
+        }
+    ];
+    return info;
 }
-function getGeoInfo(data) {
-    return new Promise((resolve, reject) => {
-        const lat = data.lat;
-        const lng = data.lon;
-        const geoURL = "https://macrostrat.org/api/geologic_units/map?lat=" + lat + "&lng=" + lng;
-        https_1.default.get(geoURL, (response) => {
-            if (response.statusCode !== 200) {
-                reject("Invalid Response Code: " + response.statusCode);
-            }
-            let responseData = "";
-            response.on("data", (data) => {
-                responseData += data;
-            });
-            response.on("end", () => {
-                const geoData = JSON.parse(responseData);
-                const geoInfo = geoData.success.data[0].name;
-                resolve(geoInfo);
-            });
-        });
+function getInformation(ipAddress, city, country, lat, lon) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let ipInfo = {};
+        if (ipAddress !== "") {
+            const ipURL = "http://ip-api.com/json/" + ipAddress;
+            ipInfo = (yield getAPIInfo(ipURL, "http"));
+            city = ipInfo.city;
+            country = ipInfo.country;
+            lat = ipInfo.lat;
+            lon = ipInfo.lon;
+        }
+        const apiKey = "APIKEYHERE";
+        const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&userCountry=" + country + "&appid=" + apiKey + "&units=imperial";
+        const weatherInfo = yield getAPIInfo(weatherURL, "https");
+        if (lat === 0 && lon === 0) {
+            lat = weatherInfo.coord.lat;
+            lon = weatherInfo.coord.lon;
+        }
+        const geoURL = "https://macrostrat.org/api/geologic_units/map?lat=" + lat + "&lng=" + lon;
+        const geoInfo = yield getAPIInfo(geoURL, "https");
+        return [ipInfo, weatherInfo, geoInfo];
     });
 }
 app.get("/", (_req, _res) => {
-    const ipAddress = _req.socket.remoteAddress;
+    let ipAddress = _req.socket.remoteAddress;
+    ipAddress = "24.48.0.1";
     if (ipAddress) {
-        getIPInfo(ipAddress).then((data) => {
-            return Promise.all([getWeatherInfo(data), getGeoInfo(data)]);
-        }).then((values) => {
-            console.log("Final results: ");
-            console.log(values);
-        }).catch((err) => {
-            console.dir(err);
+        getInformation(ipAddress)
+            .then((_data) => {
+            console.log(_data);
+            const info = formatInformation(_data);
+            _res.render("home", { info: info });
+        })
+            .catch((error) => {
+            console.log("There was an error: " + error);
         });
     }
 });
+app.post("/search", (_req, _res) => {
+    const ipAddress = "";
+    const cityCountryInput = _.replace(_req.body.searchInput, "\s", "");
+    const coordInput = _.replace(_req.body.coordInput, "\s", "");
+    const cityCountry = cityCountryInput.split(",");
+    const city = cityCountry[0];
+    const country = cityCountry[1];
+    let lat = 0;
+    let lon = 0;
+    if (coordInput !== "") {
+        const coords = coordInput.split(",");
+        lat = Number(coords[0]);
+        lon = Number(coords[1]);
+    }
+    getInformation(ipAddress, city, country, lat, lon)
+        .then((data) => {
+        console.log(data);
+        let info = formatInformation(data);
+        _res.render("home", { info: info });
+    })
+        .catch((error) => {
+        console.log("There was an error: " + error);
+    });
+});
 app.listen(port, () => {
     const countriesNowURL = "https://countriesnow.space/api/v0.1/countries";
-    https_1.default.get(countriesNowURL, (response) => {
-        console.log(response.statusCode);
-        let responseData = "";
-        response.on("data", (data) => {
-            responseData += data;
+    getAPIInfo(countriesNowURL, "https")
+        .then((data) => {
+        data.data.forEach((obj) => {
+            for (let i = 0; i < obj.cities.length; i++) {
+                const input = obj.cities[i] + "," + obj.country;
+                cityCountry.push(_.replace(input, "\s", ""));
+            }
         });
-        response.on("end", () => {
-            countries = JSON.parse(responseData);
-            countries.data.forEach((obj) => {
-                for (let i = 0; i < obj.cities.length; i++) {
-                    cityCountry.push(obj.cities[i] + ", " + obj.country);
-                }
-            });
-        });
+    })
+        .catch((error) => {
+        console.log(error);
     });
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import https from "https";
 import { IncomingMessage } from "http";
-import http from "http";
+import * as http from "http";
+import * as _ from "lodash";
 
 const app = express();
 
@@ -10,16 +11,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const port: number = 3000;
 
-type Countries = {
-    error: boolean;
-    msg: string;
-    data: [{
-        iso2: string;
-        iso3: string;
-        country: string;
-        cities: [string];
-    }]
-}
+// type Countries = {
+//     error: boolean;
+//     msg: string;
+//     data: [{
+//         iso2: string;
+//         iso3: string;
+//         country: string;
+//         cities: [string];
+//     }]
+// }
 
 type IPInfo = {
    status: string,
@@ -45,25 +46,42 @@ type IPInfo = {
 //     lng: number
 // }
 
-type Weather = {
-    main: {
-        temp: number,
-        feels_like: number,
-        temp_min: number,
-        temp_max: number,
-        pressure: number,
-        humidity: number
-    }
-}
+// type Weather = {
+//     main: {
+//         temp: number,
+//         feels_like: number,
+//         temp_min: number,
+//         temp_max: number,
+//         pressure: number,
+//         humidity: number
+//     }
+// }
 
-type GeoInfo = {
-    name: string
-}
+// type GeoInfo = {
+//     name: string
+// }
 
 
-let countries = {} as Countries;
+// let countries = {} as Countries;
 
-let cityCountry: string[] = [];
+
+
+type CityCountry = string[];
+
+// type UserInfo = {
+//     city: string,
+//     country: string,
+//     latitude: number,
+//     longitude: number
+// }
+
+//declare a type for UserWeather
+
+//declare a a type for UserGeology
+
+// let userInfo = {} as UserInfo;
+let cityCountry = [] as CityCountry;
+
 
 
 app.use(express.static('public'));
@@ -71,166 +89,180 @@ app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 
-//function for getting data from an api
-function getIPInfo(ipAddress: string){
-        
+//retrieves a response parsed to JSON for a given API url and http/s type
+function getAPIInfo(url:string, requestType:string){
+    
     return new Promise((resolve, reject)=>{
 
-        //_req.socket.remoteAddress
+        if(requestType === "https"){
+            https.get(url, (response: IncomingMessage)=>{
 
-        ipAddress = "24.48.0.1";
-
-        const url = "http://ip-api.com/json/" + ipAddress;
-        http.get(url, (response: IncomingMessage)=>{
-
-            if(response.statusCode !== 200){
-                reject("Invalid Response Code: " + response.statusCode);
-            }
-
-            let responseData: any = "";
-
-            response.on("data", (data: any)=>{
-                responseData += data;
+                if(response.statusCode !== 200){
+                    reject("Invalid Response Code: " + response.statusCode + "from " + url);
+                }
+    
+                let responseData: any = "";
+    
+                response.on("data", (data: any)=>{
+                    responseData += data;
+                });
+    
+                response.on("end", ()=>{
+    
+                    resolve(JSON.parse(responseData));
+                });
+    
             });
 
-            response.on("end", ()=>{
-                const ipInfo: IPInfo = JSON.parse(responseData);
+        } else {
+            http.get(url, (response: IncomingMessage)=>{
 
-                resolve(ipInfo);
+                if(response.statusCode !== 200){
+                    reject("Invalid Response Code: " + response.statusCode + "from " + url);
+                }
+    
+                let responseData: any = "";
+    
+                response.on("data", (data: any)=>{
+                    responseData += data;
+                });
+    
+                response.on("end", ()=>{
+    
+                    resolve(JSON.parse(responseData));
+                });
+    
             });
-
-        });
+        }
 
     });
 
 }
 
+function formatInformation(data:any): any{
 
-function getWeatherInfo(data: IPInfo){
+    //data contains an array of objects, function will return an array of objects
+    //representing each card to be sent to the front-end
 
-    return new Promise((resolve, reject)=>{
+    const info = [
+        {
+            cardTitle: "Weather",
+            listItemOne: "Temperature: " + data[1].main.temp,
+            listItemTwo: "Description: " + data[1].weather.description
 
-         //Query openweathermap's api using the city, country gathered from ip-api
-         const apiKey = "APIKEYHERE"
-         const userCity: string = data.city;
-         const userCountry: string = data.country;
-         const url = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&userCountry=" + userCountry + "&appid=" + apiKey + "&units=imperial";
+        }
+    ];
 
-         https.get(url, (response: IncomingMessage)=> {
-
-            if(response.statusCode !== 200){
-                reject("Invalid Response Code: " + response.statusCode);
-            }
-
-            let responseData: any = "";
-
-             response.on("data", (data: any)=>{
-                 responseData += data;
-             });
-
-             response.on("end", ()=>{
-
-                const jsonWeatherData: any= JSON.parse(responseData);
-
-                const weatherInfo: Weather = jsonWeatherData.main;
-
-                resolve(weatherInfo);            
-
-             });
-
-         });  
-
-    });
+    return info;
 
 }
 
-function getGeoInfo(data: IPInfo){
+//retrieves all information for each API
+async function getInformation(ipAddress: string, city?:string, country?:string, lat?:number, lon?:number){
 
-    return new Promise((resolve, reject)=>{
+    let ipInfo: IPInfo = {} as IPInfo;
 
-        const lat = data.lat;
-        const lng = data.lon;
+    //API Call to ip-api if ip-based search supplied
+    if(ipAddress !== ""){
+        const ipURL:string = "http://ip-api.com/json/" + ipAddress;
+        ipInfo = await getAPIInfo(ipURL, "http") as IPInfo;
 
-        const geoURL = "https://macrostrat.org/api/geologic_units/map?lat=" + lat + "&lng=" + lng;
-        https.get(geoURL, (response: IncomingMessage)=>{
+        city = ipInfo.city;
+        country = ipInfo.country;
+        lat = ipInfo.lat;
+        lon = ipInfo.lon;
+    }
 
-            if(response.statusCode !== 200){
-                reject("Invalid Response Code: " + response.statusCode);
-            }
+    //API Call to OpenWeatherMap
+    const apiKey = "APIKEYHERE"
+    const weatherURL:string = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&userCountry=" + country + "&appid=" + apiKey + "&units=imperial";
+    const weatherInfo: any = await getAPIInfo(weatherURL, "https");
 
-            let responseData: any = "";
+    if(lat === 0 && lon === 0){
+        lat = weatherInfo.coord.lat;
+        lon = weatherInfo.coord.lon;
+    }
 
-            response.on("data", (data: any)=>{
-                responseData += data;
-            });
+    //API Call to Macrostrat (Geology)
+    const geoURL = "https://macrostrat.org/api/geologic_units/map?lat=" + lat + "&lng=" + lon;
+    const geoInfo: any = await getAPIInfo(geoURL, "https");
 
-            response.on("end", ()=>{
-
-                const geoData = JSON.parse(responseData);
-
-                const geoInfo: GeoInfo = geoData.success.data[0].name;
-
-                resolve(geoInfo);
-            });
-
-        });
-
-    });
-
+    return [ipInfo, weatherInfo, geoInfo];
 }
 
 
 app.get("/", (_req: express.Request, _res: express.Response)=>{
 
-    const ipAddress = _req.socket.remoteAddress;
+    //TODO: if we already have the information for the user's current location, make no calls and return it
+
+    let ipAddress = _req.socket.remoteAddress;
+    ipAddress = "24.48.0.1";
 
     if(ipAddress){
 
-        //if IP address could be found, we'll get IPInfo (city, country, lat, lng) and use that information to make requests
-        //to the openweather and macrostrat APIs
-        getIPInfo(ipAddress).then((data: any)=>{
-            return Promise.all([getWeatherInfo(data), getGeoInfo(data)]);
-    
-        }).then((values)=>{
-    
-            console.log("Final results: ");
-            console.log(values);
-            //render the page using the weather, geologic, and location information
-
-
-        }).catch((err) => {
-            console.dir(err);
-        });
+        getInformation(ipAddress)
+            .then((_data)=>{
+                console.log(_data);
+                //format the data and render
+                const info:any = formatInformation(_data);
+                _res.render("home", {info: info});
+            })
+            .catch((error)=>{
+                console.log("There was an error: " + error);
+            });
     }
 
 });
 
+app.post("/search", (_req: express.Request, _res: express.Response)=>{
+
+    const ipAddress = "";
+
+    //TODO: get the search parameters (city, country or lat/lon)
+    const cityCountryInput:string = _.replace(_req.body.searchInput, "\s", "");
+    const coordInput:string = _.replace(_req.body.coordInput, "\s", "");
+
+    const cityCountry: string[] = cityCountryInput.split(",");
+    const city = cityCountry[0];
+    const country = cityCountry[1];
+
+    let lat:number = 0;
+    let lon:number = 0;
+
+    if(coordInput !== ""){
+        const coords: string[] = coordInput.split(",");
+        lat = Number(coords[0]);
+        lon = Number(coords[1]);
+    }
+
+    getInformation(ipAddress, city, country, lat, lon)
+        .then((data)=>{
+            console.log(data);
+            let info = formatInformation(data);
+            _res.render("home", {info: info});
+        })
+        .catch((error)=>{
+            console.log("There was an error: " + error);
+        });
+});
+
 app.listen(port, ()=>{
 
-    //Retrieve country/city data from countriesnow api
-    const countriesNowURL = "https://countriesnow.space/api/v0.1/countries"
-
-    https.get(countriesNowURL, (response: IncomingMessage)=>{
-        console.log(response.statusCode);
-
-        let responseData: any = "";
-
-        response.on("data", (data: any)=>{
-            responseData += data;
-        });
-
-        response.on("end", ()=> {
-            countries = JSON.parse(responseData);
-
-            countries.data.forEach((obj)=>{
+    //API call to countriesnow api to get all countries/cities
+    const countriesNowURL: string = "https://countriesnow.space/api/v0.1/countries"
+    getAPIInfo(countriesNowURL, "https")
+        .then((data: any)=>{
+            data.data.forEach((obj:any)=>{
                 
                 for(let i = 0; i < obj.cities.length; i++){
-                    cityCountry.push(obj.cities[i] + ", " + obj.country);
+                    const input = obj.cities[i] + "," + obj.country;
+                    cityCountry.push(_.replace(input, "\s", ""));
                 }
 
             });
+        })
+        .catch((error)=>{
+            console.log(error);
         });
-
-    });
 
 });
